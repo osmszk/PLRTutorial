8cb9733bad7db3720e9fb56e1564c12e9dacecd2
osuzuki <osuzuki@plegineer.com>
2014-02-16 12:48:30 +0900

    Removed unnecessary srcs

---
+++ TheTutorial/PLRViewController.m
@@ -0,0 +1,38 @@
+//
+//  PLRViewController.m
+//  TheTutorial
+//
+//  Created by Osamu Suzuki on 2014/02/16.
+//  Copyright (c) 2014年 Plegineer, Inc. All rights reserved.
+//
+
+#import "PLRViewController.h"
+
+@interface PLRViewController ()
+
+@end
+
+@implementation PLRViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
